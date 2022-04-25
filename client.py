a5c186b21204a390b855995cc6526d067bb219cd
LokiReborn11 <frogs7266@gmail.com>
2022-04-24 21:13:39 -0400

    upload+download

--- client.py
+++ client.py
@@ -1,22 +1,65 @@
+import os
 import socket
 
 
-def client_connect():
-  client_socket = socket.socket()
-  port = 2048
-  client_socket.connect(('127.0.0.1', port))
-  print (client_socket.recv(1024).decode())
-  client_socket.close()
+
+IP = "localhost"
+PORT = 2048
+ADDR = (IP,PORT)
+SIZE = 1024 ## byte .. buffer size
+FORMAT = "utf-8"
+SERVER_DATA_PATH = "server_data"
+
+
+def main():
+  client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+  client.connect(ADDR)
+  while True:  ### multiple communications
+        data = client.recv(SIZE).decode(FORMAT)
+        cmd, msg = data.split("@")
+        if cmd == "OK":
+            print(f"{msg}")
+        elif cmd == "DISCONNECTED":
+            print(f"{msg}")
+            break
+        
+        data = input("Input a command[UPLOAD(UPLOAD_filename), DOWNLOAD(DOWNLOAD_filename), DELETE, DIR, CLOSESERVER, DISCONNECT]> ")
+        data2 = data 
+        data = data.split("_")
+        cmd = data[0]
+
+        if cmd == "DISCONNECT":
+            client.send(cmd.encode(FORMAT))
+
+        elif cmd == "CLOSESERVER":
+            client.send(cmd.encode(FORMAT))
+            break
+
+        elif cmd == "UPLOAD":
+            client.send(data2.encode(FORMAT))
+            file = open(data[1], "rb")
+            sendData = file.read(1024)
+            client.send(sendData)
+            print("Data packet sent")
+            file.close()
+            print("File successfully sent")
+        
+        elif cmd == "DOWNLOAD":
+          client.send(data2.encode(FORMAT))
+          file = open(data[1], 'wb')
+          recData = client.recv(SIZE)
+          file.write(recData)
+          print("Data packet received")
+          file.close()
+
+    
+          
+        
+
+
+
+  print("Disconnected from the server.")
+  client.close() ## close the connection
 
 if __name__ == '__main__':
-    keepGoing = True
-    while keepGoing:
-      userinp = input("Input one of the following commands(CONNECT, UPLOAD, DOWNLOAD, DELETE, DIR, END):")
-      if userinp == "CONNECT":
-        client_connect()
-      elif userinp == "END":
-        keepGoing = False
-        print("Goodbye!")
-      else:
-        print("Invalid command")
-
+    main()
--- server.py
+++ server.py
@@ -1,17 +1,61 @@
 import socket
+import os
+import threading
 
-def server_program():
-  server_socket = socket.socket()  
-  server_socket.bind(('', 2048))
-  print("Socket created")
-  server_socket.listen(5)
+IP = "localhost"
+PORT = 2048
+ADDR = (IP,PORT)
+SIZE = 1024
+FORMAT = "utf-8"
+SERVER_PATH = "server"
 
-  while True:
-    (client_socket, address) = server_socket.accept()
-    print ('Got connection from ', address )
-    client_socket.send('Thank you for connecting'.encode())
-    client_socket.close()
-    break
+def server_program(conn, addr):
+  serverConnect = True
+  print(f"[NEW CONNECTION] {addr} connected.")
+  conn.send("OK@Welcome to the server".encode(FORMAT))
+  while serverConnect:
+    data =  conn.recv(SIZE).decode(FORMAT)
+    data = data.split("_")
+    cmd = data[0]
+    
+    send_data = "OK@"
+    if cmd == "CLOSESERVER":
+      serverConnect = False
+      #server_socket.close()
+      print("Shutting server down")
+      break
+    elif cmd == "DISCONNECT":
+      serverConnect = False
+      conn.send("DISCONNECTED@Closing server connection".encode(FORMAT))
+      break
+    elif cmd == "UPLOAD":
+      file = open(os.path.join("./serverStorage",data[1]), 'wb')
+      recData = conn.recv(SIZE)
+      file.write(recData)
+      print("Data packet received")
+      file.close()
+      conn.send("OK@File successfully uploaded!".encode(FORMAT))
+    elif cmd == "DOWNLOAD":
+      file = open(os.path.join("./serverStorage",data[1]), 'rb')
+      sendData = file.read(1024)
+      conn.send(sendData)
+      print("Data packet sent")
+      file.close()
+      print("File successfully sent")
+      conn.send("OK@File successfully downloaded!".encode(FORMAT))
+
+  print(f"{addr} disconnected")
+  conn.close()
+    
+
   
 if __name__ == '__main__':
-    server_program()
+  print("Starting server")
+  server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)  
+  server_socket.bind(ADDR)
+  server_socket.listen()
+  print(f"server is listening on {IP}: {PORT}")
+  while True:
+    conn, addr = server_socket.accept() ### accept a connection from a client
+    thread = threading.Thread(target = server_program, args = (conn, addr)) ## assigning a thread for each client
+    thread.start()
